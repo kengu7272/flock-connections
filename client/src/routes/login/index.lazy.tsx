39c6995c6123f718e76ad2e8de1a0a1ea0d3f0bb
kengu7272 <kengu72@gmail.com>
2024-03-06 00:51:27 -0500

    fix: redirect to /flock upon login

--- client/src/routes/login/index.lazy.tsx
+++ client/src/routes/login/index.lazy.tsx
@@ -9,7 +9,7 @@ function Login() {
     <div className='flex'>
       <main className='mx-auto flex flex-col gap-10 justify-center mt-32 rounded-lg bg-slate-700 py-6 px-32'>
         <h2 className='text-xl'>Login with Google</h2>
-        <a href='/api/login/google' className='bg-red-500 hover:bg-red-600 active:bg-red-700 rounded-lg px-6 py-3 mx-auto'>Google</a>
+        <a href='/api/login/google' className='bg-red-600 hover:bg-red-700 active:bg-red-800 text-white rounded-lg px-6 py-3 mx-auto'>Google</a>
       </main>
     </div>
   )
