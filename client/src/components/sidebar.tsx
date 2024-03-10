39c41dca945e962b4936a2b58f74aafdf5aeb264
kengu7272 <kengu72@gmail.com>
2024-03-10 01:55:29 -0500

    feat: username

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -2,18 +2,24 @@ import { useState } from "react";
 import { Link } from "@tanstack/react-router";
 import { clsx } from "clsx";
 import { Home, LogOut, Menu, PersonStanding } from "lucide-react";
+import { trpc } from "~/client/utils/trpc";
 
 export default function Sidebar() {
+  const { data: loggedInData } = trpc.base.loggedIn.useQuery();
+  
   const [mobileLinks, setMobileLinks] = useState(false);
-  const linkClass =
-    clsx({"rounded-lg py-3 text-center hover:bg-slate-700 lg:flex items-center justify-center gap-2 border border-slate-600 hidden": true, "!flex": mobileLinks });
+  const linkClass = clsx({
+    "rounded-lg py-3 text-center hover:bg-slate-700 lg:flex items-center justify-center gap-2 border border-slate-600 hidden":
+      true,
+    "!flex": mobileLinks,
+  });
 
   const linkOnClick = () => setMobileLinks(false);
 
   return (
     <div
       className={clsx({
-        "fixed z-10 flex flex-col items-center justify-center gap-16 whitespace-nowrap py-6 lg:h-screen px-4 lg:w-72 lg:bg-slate-800":
+        "fixed z-10 flex flex-col items-center justify-center gap-16 whitespace-nowrap px-4 py-6 lg:h-screen lg:w-72 lg:bg-slate-800":
           true,
         "h-screen min-w-72 bg-slate-800": mobileLinks,
       })}
@@ -28,9 +34,17 @@ export default function Sidebar() {
       >
         <Menu />
       </button>
-      <Link to="/" className={clsx({"absolute hover:text-slate-300 top-12 text-2xl font-bold hidden lg:block": true, "!block": mobileLinks})}>
-        Flock Connections
-      </Link>
+      <div
+        className={clsx({
+          "absolute flex-col items-center top-12 hidden lg:flex": true,
+          "!flex": mobileLinks,
+        })}
+      >
+        <Link to="/" className="text-2xl hover:text-slate-300">
+          Flock Connections
+        </Link>
+        <span className="text-xl text-slate-300 font-semibold">{loggedInData?.username}</span>
+      </div>
       <div className="flex w-full flex-col gap-2">
         <Link onClick={linkOnClick} className={linkClass} to="/flock">
           <Home />
