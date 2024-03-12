f4b950af6248b82c96c8127cd115408e5b11eb68
kengu7272 <kengu72@gmail.com>
2024-03-12 01:45:28 -0400

    feat: check if query is loading before loading prompt

--- client/src/routes/_auth/flock/index.lazy.tsx
+++ client/src/routes/_auth/flock/index.lazy.tsx
@@ -7,17 +7,17 @@ export const Route = createLazyFileRoute("/_auth/flock/")({
 });
 
 function Flock() {
-  const { data: group } = trpc.user.getGroup.useQuery();
+  const data = trpc.user.getGroup.useQuery();
 
   return (
     <div className="flex w-full justify-center py-24">
       <main>
-        {!group && (
+        {!data.isLoading && !data.data?.group && (
           <div className="flex flex-col items-center gap-4 rounded-lg border border-slate-600 p-4">
-            <h3 className="text-2xl font-bold">You aren't part of a group</h3>
+            <h3 className="text-2xl font-bold">You aren't part of a Flock</h3>
             <span>Create one or wait for an invite</span>
             <Link to="/group" className="rounded-md font-semibold bg-sky-500 px-3 py-2 text-white hover:bg-sky-600 active:bg-sky-700">
-              Create a Group
+              Create a Flock
             </Link>
           </div>
         )}
