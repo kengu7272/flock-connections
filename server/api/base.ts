8582808eea83d52f9625a4c125fe1ce6b17f6c82
kengu7272 <kengu72@gmail.com>
2024-03-19 02:56:49 -0400

    feat: uploadthing && update profile picture (WIP)

--- server/api/base.ts
+++ server/api/base.ts
@@ -1,16 +1,30 @@
 import { Hono } from "hono";
+import { createRouteHandler } from "uploadthing/server";
 
 import { lucia } from "../auth";
-import { Session, User } from "../db/src/types";
+import { Flock, Session, User } from "../db/src/types";
+import { uploadRouter } from "../uploadthing.ts";
 import login from "./login.ts";
+
+const { GET, POST } = createRouteHandler({
+  router: uploadRouter,
+});
 
 const hono = new Hono<{
   Variables: {
     user: User | null;
+    flock: Flock | null;
     session: Session | null;
   };
 }>();
 
+//uploadthing
+const ut = hono
+  .get("/", (context) => GET(context.req.raw))
+  .post("/", (context) => POST(context.req.raw));
+hono.route("/uploadthing", ut);
+
+//routes
 hono.get("/logout", async (c) => {
   await lucia.invalidateSession(c.get("session")?.id ?? "");
   c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), {
